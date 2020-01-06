e0be0e979d760f5667be0cccd4a1112ce7f28a25
Jamie Wiebe <jamie.wiebe@finn.no>
2020-01-06 16:13:06 +0100

    initial commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/j18e/sparing
+
+go 1.13
+
+require (
+	github.com/joho/godotenv v1.3.0
+	github.com/kelseyhightower/envconfig v1.4.0
+	golang.org/x/oauth2 v0.0.0-20191202225959-858c2ad4c8b6
+)
---
+++ main.go
@@ -0,0 +1,170 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/joho/godotenv"
+	"github.com/kelseyhightower/envconfig"
+	"golang.org/x/oauth2/clientcredentials"
+)
+
+const (
+	API_SERVER = "https://api.sbanken.no"
+	TOKEN_URL  = "https://auth.sbanken.no/identityserver/connect/token"
+)
+
+type Client struct {
+	cli        *http.Client
+	customerID string
+	accountID  string
+}
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("loading .env file: %v", err)
+	}
+	var envVars struct {
+		AccountID    string `required:"true" envconfig:"ACCOUNT_ID"`
+		CustomerID   string `required:"true" envconfig:"CUSTOMER_ID"`
+		ClientID     string `required:"true" envconfig:"CLIENT_ID"`
+		ClientSecret string `required:"true" envconfig:"CLIENT_SECRET"`
+	}
+	if err := envconfig.Process("", &envVars); err != nil {
+		log.Fatal(err)
+	}
+
+	conf := clientcredentials.Config{
+		ClientID:     envVars.ClientID,
+		ClientSecret: envVars.ClientSecret,
+		TokenURL:     TOKEN_URL,
+	}
+
+	httpCli := conf.Client(context.TODO())
+	httpCli.Timeout = 10 * time.Second
+
+	cli := Client{
+		cli:        httpCli,
+		customerID: envVars.CustomerID,
+		accountID:  envVars.AccountID,
+	}
+
+	acctID := ""
+	accounts, err := cli.getAccounts()
+	if err != nil {
+		log.Fatalf("getting accounts: %v", err)
+	}
+	for _, acct := range accounts {
+		if acct.Name == "main" {
+			acctID = acct.ID
+			break
+		}
+	}
+	if acctID == "" {
+		log.Fatal("couldn't find account 'main'")
+	}
+
+	transactions, err := cli.getTransactions(acctID)
+	if err != nil {
+		log.Fatalf("getting transactions: %v", err)
+	}
+	for _, trans := range transactions {
+		if trans.CardDetails != nil {
+			fmt.Println(trans)
+		}
+	}
+}
+
+func (c *Client) callAPI(path string) ([]byte, error) {
+	var bs []byte
+
+	req, _ := http.NewRequest("GET", API_SERVER+path, nil)
+	req.Header.Set("customerId", c.customerID)
+
+	res, err := c.cli.Do(req)
+	if err != nil {
+		return bs, fmt.Errorf("calling Sbanken API: %w", err)
+	}
+
+	if res.StatusCode > 399 {
+		bs, _ = ioutil.ReadAll(res.Body)
+		return bs, fmt.Errorf("status %d: %s", res.StatusCode, bs)
+	}
+
+	bs, err = ioutil.ReadAll(res.Body)
+	if err != nil {
+		return bs, fmt.Errorf("reading response: %w", err)
+	}
+	return bs, nil
+}
+
+type Transaction struct {
+	Date        string       `json:"accountingDate"`
+	Amount      float64      `json:"amount"`
+	Type        string       `json:"transactionType"`
+	Text        string       `json:"text"`
+	CardDetails *CardDetails `json:"cardDetails"`
+}
+
+type CardDetails struct {
+	Card             string `json:"cardNumber"`
+	MerchantCategory string `json:"merchangeCategoryDescription"`
+	MerchantCity     string `json:"merchantCity"`
+	MerchantName     string `json:"merchantName"`
+	OriginalCurrency string `json:"originalCurrencyCode"`
+	PurchaseDate     string `json:"purchaseDate"`
+	TransactionID    string `json:"transactionId"`
+}
+
+func (c *Client) getTransactions(acctID string) ([]Transaction, error) {
+	bs, err := c.callAPI("/exec.bank/api/v1/Transactions/" + acctID)
+	if err != nil {
+		return nil, err
+	}
+
+	var res struct {
+		Length *int          `json:"availableItems"`
+		Items  []Transaction `json:"items"`
+	}
+	if err := json.Unmarshal(bs, &res); err != nil {
+		return nil, fmt.Errorf("unmarshaling json: %w", err)
+	}
+
+	if res.Length == nil {
+		return nil, fmt.Errorf("missing field \"availableItems\" in response")
+	}
+	return res.Items, nil
+}
+
+type Account struct {
+	ID          string  `json:"accountId"`
+	Number      string  `json:"accountNumber"`
+	CustomerID  string  `json:"ownerCustomerId"`
+	Name        string  `json:"name"`
+	Type        string  `json:"accountType"`
+	Available   float64 `json:"available"`
+	Balance     float64 `json:"balance"`
+	CreditLimit float64 `json:"creditLimit"`
+}
+
+func (c *Client) getAccounts() ([]Account, error) {
+	var accounts []Account
+	bs, err := c.callAPI("/exec.bank/api/v1/Accounts")
+	if err != nil {
+		return accounts, err
+	}
+
+	var accountsRes struct {
+		Items []Account `json:"items"`
+	}
+	if err := json.Unmarshal(bs, &accountsRes); err != nil {
+		return accounts, fmt.Errorf("unmarshaling json: %w", err)
+	}
+
+	return accountsRes.Items, nil
+}
