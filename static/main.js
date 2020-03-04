9a547a2dd11cea21370fbfeb631216a59b740f80
Jamie Wiebe <jamie@j18e.com>
2020-03-04 16:51:26 +0100

    fix js

--- static/main.js
+++ static/main.js
@@ -1,40 +1,49 @@
-function addToSelectors() {
-  sel = document.getElementById('date-selector');
+function addToSelectors(id, cellType) {
+  sel = document.getElementById(id);
   rows = document.getElementById('spending-table').getElementsByClassName("table-row");
-  dates = [];
+  items = [];
 
   for (var i = 0; i < rows.length; i++) {
-    date = rows[i].getElementsByClassName("date-cell")[0].textContent;
-    if (dates.includes(date)) {
+    item = rows[i].getElementsByClassName(cellType)[0].textContent;
+    if (items.includes(item)) {
       continue
     }
-    dates.push(date);
+    items.push(item);
   }
 
-  for (var i = 0; i < dates.length; i++) {
+  for (var i = 0; i < items.length; i++) {
     opt = document.createElement('option');
-    opt.value = dates[i];
-    opt.innerHTML = dates[i];
+    opt.value = items[i];
+    opt.innerHTML = items[i];
     sel.appendChild(opt);
   }
 }
 
-function logSomething() {
-  console.log('test log');
-}
-
-function selectOpt(sel) {
+function selectOpt(sel, cellType) {
   val = sel.options[sel.selectedIndex].value;
-  console.log(sel.options.length);
   rows = document.getElementById("spending-table").getElementsByClassName("table-row");
+
   for (var i = 0; i < rows.length; i++) {
     if (val == 'all') {
       rows[i].style.display = '';
-    } else if (rows[i].getElementsByClassName("date-cell")[0].textContent != val) {
+    } else if (rows[i].getElementsByClassName(cellType)[0].textContent != val) {
       rows[i].style.display = "none";
     } else {
       rows[i].style.display = '';
     }
   }
-  // div.style.display = "none";
+  updateTotal();
+}
+
+function updateTotal() {
+  var total = 0;
+  sel = document.getElementById('spending-total');
+  rows = document.getElementById("spending-table").getElementsByClassName("table-row");
+  for (var i = 0; i < rows.length; i++) {
+    if (rows[i].style.display == 'none') {
+      continue;
+    }
+    total += parseInt(rows[i].getElementsByClassName("nok-cell")[0].textContent);
+  }
+  sel.textContent = 'Total: ' + total + ' NOK';
 }
